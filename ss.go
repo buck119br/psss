c3b61770ce9af0be0cf67c77198a95a83f02b9f6
刘春旭 <liuchunxu@liuchunxudeMac-mini.local>
2017-06-06 17:47:29 +0800

    change file

--- ss.go
+++ ss.go
@@ -11,6 +11,9 @@ import (
 const (
 	UserHz = 3
 
+	Sockstat4Path = "/proc/net/sockstat"
+	Sockstat6Path = "/proc/net/sockstat6"
+
 	TCPv4Path = "/proc/net/tcp"
 	TCPv6Path = "/proc/net/tcp6"
 	UDPv4Path = "/proc/net/udp"
@@ -26,10 +29,19 @@ const (
 )
 
 var (
+	Summary map[string]map[string]int
+
 	GlobalTCPv4Records map[uint64]*GenericRecord
 	GlobalTCPv6Records map[uint64]*GenericRecord
 	GlobalUDPv4Records map[uint64]*GenericRecord
 	GlobalUDPv6Records map[uint64]*GenericRecord
+
+	Protocal = []string{
+		"RAW",
+		"UDP",
+		"TCP",
+		"FRAG",
+	}
 
 	Sstate = []string{
 		"UNKNOWN",
@@ -378,6 +390,103 @@ func GenericRecordRead(family string) (err error) {
 			GlobalUDPv4Records[record.Inode] = record
 		case UDPv6Str:
 			GlobalUDPv6Records[record.Inode] = record
+		}
+	}
+	return nil
+}
+
+func GetSocketCount(fields []string) (int, error) {
+	for i := range fields {
+		if fields[i] == "inuse" {
+			return strconv.Atoi(fields[i+1])
+		}
+	}
+	return 0, nil
+}
+
+// IPv6:versionFlag = true; IPv4:versionFlag = false
+func GenericReadSockstat(versionFlag bool) (err error) {
+	var (
+		file      *os.File
+		line      string
+		fields    []string
+		tempCount int
+	)
+	if versionFlag {
+		file, err = os.Open(Sockstat6Path)
+	} else {
+		file, err = os.Open(Sockstat4Path)
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if err = scanner.Err(); err != nil {
+			fmt.Println(err)
+			return err
+		}
+		line = scanner.Text()
+		fields = strings.Fields(line)
+		switch fields[0] {
+		case "sockets:":
+			continue
+		case "TCP:":
+			if Summary["TCP"][IPv4String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "TCP6:":
+			if Summary["TCP"][IPv6String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "UDP:":
+			if Summary["UDP"][IPv4String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "UDP6:":
+			if Summary["UDP"][IPv6String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "UDPLITE:":
+			if tempCount, err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+			Summary["UDP"][IPv4String] += tempCount
+		case "UDPLITE6:":
+			if tempCount, err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+			Summary["UDP"][IPv6String] += tempCount
+		case "RAW:":
+			if Summary["RAW"][IPv4String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "RAW6:":
+			if Summary["RAW"][IPv6String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "FRAG:":
+			if Summary["FRAG"][IPv4String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		case "FRAG6:":
+			if Summary["FRAG"][IPv6String], err = GetSocketCount(fields[1:]); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		default:
+			continue
 		}
 	}
 	return nil
