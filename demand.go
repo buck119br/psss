748d9527030fa997a55562547d8058226c7f28f7
刘春旭 <liuchunxu@liuchunxudeMac-mini.local>
2017-07-04 12:07:43 +0800

    change demand's data struct

--- demand.go
+++ demand.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"strings"
 
+	"encoding/json"
 	"golang.org/x/sys/unix"
 )
 
@@ -19,35 +20,55 @@ func isHostLocal(host string) bool {
 	return false
 }
 
-type topology struct {
-	employer map[string]bool
-	employee map[string]bool
-	ports    map[IP]bool
+type DemandProcInfo struct {
+	State     string  `json:"state"`
+	StartTime string  `json:"starttime"`
+	LoadAvg   float64 `json:"loadavg"`
+	VmSize    uint64  `json:"vmsize"`
+	VmRSS     uint64  `json:"vmrss"`
 }
 
-func newtopology() *topology {
-	t := new(topology)
-	t.employer = make(map[string]bool)
-	t.employee = make(map[string]bool)
-	t.ports = make(map[IP]bool)
+type ListenTopology struct {
+	ProcInfo DemandProcInfo  `json:"procinfo"`
+	Clients  map[string]bool `json:"clients"`
+	Upstream map[string]bool `json:"upstream"`
+	Ports    map[IP]bool     `json:"ports"`
+}
+
+func newListenTopology() *ListenTopology {
+	t := new(ListenTopology)
+	t.Clients = make(map[string]bool)
+	t.Upstream = make(map[string]bool)
+	t.Ports = make(map[IP]bool)
+	return t
+}
+
+type EstabTopology struct {
+	ProcInfo DemandProcInfo `json:"procinfo"`
+	Ports    map[IP]int     `json:"ports"`
+}
+
+func newEstabTopology() *EstabTopology {
+	t := new(EstabTopology)
+	t.Ports = make(map[IP]int)
 	return t
 }
 
 type demand struct {
-	Listen map[string]*topology
-	Estab  map[string]map[IP]int
+	Listen map[string]*ListenTopology `json:"listen"`
+	Estab  map[string]*EstabTopology  `json:"estab"`
 }
 
 func newdemand() *demand {
 	d := new(demand)
-	d.Listen = make(map[string]*topology)
-	d.Estab = make(map[string]map[IP]int)
+	d.Listen = make(map[string]*ListenTopology)
+	d.Estab = make(map[string]map[IP]*EstabTopology)
 	return d
 }
 
 func (d *demand) isPortListening(port string) (bool, string) {
 	for name, topo := range d.Listen {
-		for ip := range topo.ports {
+		for ip := range topo.Ports {
 			if port == ip.Port {
 				return true, name
 			}
@@ -87,9 +108,9 @@ func (d *demand) data() {
 		for _, record := range records {
 			if record.Status == SsLISTEN {
 				if _, ok = d.Listen[record.UserName]; !ok {
-					d.Listen[record.UserName] = newtopology()
+					d.Listen[record.UserName] = newListenTopology()
 				}
-				d.Listen[record.UserName].ports[record.LocalAddr] = true
+				d.Listen[record.UserName].Ports[record.LocalAddr] = true
 			}
 		}
 	}
@@ -104,18 +125,18 @@ func (d *demand) data() {
 						for _, grecord := range grecords {
 							if grecord.LocalAddr.Port == record.RemoteAddr.Port && isHostLocal(record.RemoteAddr.Host) {
 								if isLocalListening {
-									d.Listen[record.UserName].employer[grecord.UserName] = true
+									d.Listen[record.UserName].Clients[grecord.UserName] = true
 								} else {
-									d.Listen[record.UserName].employee[grecord.UserName] = true
+									d.Listen[record.UserName].Upstream[grecord.UserName] = true
 								}
 								goto next
 							}
 						}
 					}
 					if isLocalListening {
-						d.Listen[record.UserName].employer[record.RemoteAddr.String()] = true
+						d.Listen[record.UserName].Clients[record.RemoteAddr.String()] = true
 					} else {
-						d.Listen[record.UserName].employee[record.RemoteAddr.String()] = true
+						d.Listen[record.UserName].Upstream[record.RemoteAddr.String()] = true
 					}
 					goto next
 				}
@@ -126,13 +147,15 @@ func (d *demand) data() {
 					}
 				}
 				if _, ok = d.Estab[record.UserName]; !ok {
-					d.Estab[record.UserName] = make(map[IP]int)
+					d.Estab[record.UserName] = make(map[IP]*DemandProcInfo)
+					d.Estab[record.UserName] = newEstabTopology()
 				}
-				d.Estab[record.UserName][record.RemoteAddr] = d.Estab[record.UserName][record.RemoteAddr] + 1
+				d.Estab[record.UserName].Ports[record.RemoteAddr] = d.Estab[record.UserName].Ports[record.RemoteAddr] + 1
 			next:
 			}
 		}
 	}
+
 }
 
 func (d *demand) show() {
@@ -152,19 +175,19 @@ func (d *demand) show() {
 			proc.Stat.MeminfoPrint()
 		}
 		fmt.Println("\t\tPorts")
-		for ip := range ipmap.ports {
+		for ip := range ipmap.Ports {
 			fmt.Println("\t\t\t", ip.String())
 		}
-		if len(ipmap.employer) > 0 {
+		if len(ipmap.Clients) > 0 {
 			fmt.Println("\t\tClients")
-			for v := range ipmap.employer {
+			for v := range ipmap.Clients {
 				fmt.Println("\t\t\t", v)
 			}
 		}
-		if len(ipmap.employee) > 0 {
+		if len(ipmap.Upstream) > 0 {
 			fmt.Println("\t\tUpstream")
 			serviceSet := make(map[string]bool)
-			for v := range ipmap.employee {
+			for v := range ipmap.Upstream {
 				if _, ok = serviceSet[v]; ok {
 					continue
 				}
@@ -174,7 +197,7 @@ func (d *demand) show() {
 		}
 	}
 	fmt.Println("Estab")
-	for name, ips := range d.Estab {
+	for name, topo := range d.Estab {
 		fmt.Println("\t", name)
 		fmt.Println("\t\tProcInfo")
 		for _, proc := range GlobalProcInfo[name] {
@@ -187,8 +210,10 @@ func (d *demand) show() {
 			proc.Stat.MeminfoPrint()
 		}
 		fmt.Println("\t\tRemote")
-		for ip, count := range ips {
+		for ip, count := range topo.Ports {
 			fmt.Printf("\t\t\t%s (count:%d)\n", ip.String(), count)
 		}
 	}
+
+	fmt.Println(json.Marshal(*demand))
 }
